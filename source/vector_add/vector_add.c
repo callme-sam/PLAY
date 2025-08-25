57c50f8036f2bf15a1bf041e8a41118ab4fd3373
callme-sam <samuele.colmi@chips.it>
2025-08-25 15:29:28 +0200

    vector_add: implement vector_add serial and parallel version
    Implement both serial and parallel version of vector_add, together with
    its test environment and python scripts to generate test data

---
+++ source/vector_add/vector_add.c
@@ -0,0 +1,50 @@
+#include "play.h"
+
+#include "pmsis.h"
+
+#ifdef CLUSTER
+
+static int vector_add_parallel(const float *src_a, const float *src_b, float *dst, const int len)
+{
+    int block;
+    int start;
+    int left;
+    int end;
+    int id;
+
+    id = pi_core_id();
+    block = len / NUM_CORES;
+    left = len % NUM_CORES;
+    start = id * block + (id < left ? id : left);
+    end = start + block + (id < left ? 1 : 0);
+
+    for (int i = start; i < end; i++)
+        dst[i] = src_a[i] + src_b[i];
+
+    return 0;
+}
+
+#else   /* CLUSTER */
+
+static int vector_add_serial(const float *src_a, const float *src_b, float *dst, const int len)
+{
+    for (int i = 0; i < len; i++)
+        dst[i] = src_a[i] + src_b[i];
+
+    return 0;
+}
+
+#endif  /* CLUSTER */
+
+int vector_add(const float *src_a, const float *src_b, float *dst, const int len)
+{
+    int ret;
+
+#ifdef CLUSTER
+    ret = vector_add_parallel(src_a, src_b, dst, len);
+#else   /* CLUSTER */
+    ret = vector_add_serial(src_a, src_b, dst, len);
+#endif  /* CLUSTER */
+
+    return ret;
+}
