4ea37b73982ddeddb4466a3d1684782a97d6285b
callme-sam <samuele.colmi@chips.it>
2025-08-29 15:55:13 +0200

    test/common/stats: Add more performance counters to stats

---
+++ test/common/stats.c
@@ -0,0 +1,36 @@
+#include "stats.h"
+
+#include <stdio.h>
+
+void print_stats(unsigned long _cycles, unsigned long _active, unsigned long _instr,
+                unsigned long _ldstall, unsigned long _jrstall, unsigned long _imiss,
+                unsigned long _ld, unsigned long _st, unsigned long _jump,
+                unsigned long _branch, unsigned long _btaken, unsigned long _rvc,
+                unsigned long _ld_ext, unsigned long _st_ext, unsigned long _ld_ext_cyc,
+                unsigned long _st_ext_cyc, unsigned long _tcdm_cont)
+{
+    int id;
+
+    id = pi_core_id();
+
+    printf("[%d] cycles:\t\t\t%lu\n", id, _cycles/REPEAT);
+    printf("[%d] act cycles:\t\t\t%lu\n", id, _active/REPEAT);
+    printf("[%d] instr execd:\t\t%lu\n", id, _instr/REPEAT);
+    printf("[%d] load stall:\t\t\t%lu\n", id, _ldstall/REPEAT);
+    printf("[%d] jump reg stall:\t\t%lu\n", id, _jrstall/REPEAT);
+    printf("[%d] instr miss:\t\t\t%lu\n", id, _imiss/REPEAT);
+    printf("[%d] loads execd:\t\t%lu\n", id, _ld/REPEAT);
+    printf("[%d] stores execd:\t\t%lu\n", id, _st/REPEAT);
+    printf("[%d] jump:\t\t\t%lu\n", id, _jump/REPEAT);
+    printf("[%d] branches:\t\t\t%lu\n", id, _branch/REPEAT);
+    printf("[%d] taken branches:\t\t%lu\n", id, _btaken/REPEAT);
+    printf("[%d] comprssd inst execd:\t%lu\n", id, _rvc/REPEAT);
+    printf("[%d] ext loads execd:\t\t%lu\n", id, _ld_ext/REPEAT);
+    printf("[%d] ext loads cycles:\t\t%lu\n", id, _ld_ext_cyc/REPEAT);
+    printf("[%d] ext stores execd:\t\t%lu\n", id, _st_ext/REPEAT);
+    printf("[%d] ext stores cycles:\t\t%lu\n", id, _st_ext/REPEAT);
+    printf("[%d] TDCM contentions:\t\t%lu\n", id, _tcdm_cont/REPEAT);
+    printf("[%d] IPC:\t\t\t%f\n", id, (float) _instr/_cycles);
+
+    char filename[100];
+}
--- test/common/stats.h
+++ test/common/stats.h
@@ -6,7 +6,15 @@
 #define HOTTING 2
 #define REPEAT  5
 
-#ifdef STATS
+void print_stats(unsigned long _cycles, unsigned long _active, unsigned long _instr, unsigned long _ldstall,
+                unsigned long _jrstall, unsigned long _imiss, unsigned long _ld, unsigned long _st,
+                unsigned long _jump, unsigned long _branch, unsigned long _btaken, unsigned long _rvc,
+                unsigned long _ld_ext, unsigned long _st_ext, unsigned long _ld_ext_cyc, unsigned long _st_ext_cyc,
+                unsigned long _tcdm_cont);
+
+#ifdef  STATS
+
+#ifdef  CLUSTER
 
 /**
  * @brief Initializes performance counters and begins the measurement loop.
@@ -14,22 +22,43 @@
  * @note This macro must be called first, before START_STATS() and STOP_STATS().
  */
 #define INIT_STATS()    \
-    unsigned long _cycles   = 0;    \
-    unsigned long _active   = 0;    \
-    unsigned long _instr    = 0;    \
-    unsigned long _ldstall  = 0;    \
-    unsigned long _imiss    = 0;    \
-    unsigned long _ld       = 0;    \
-    unsigned long _st       = 0;    \
-                                    \
+    unsigned long _cycles       = 0;    \
+    unsigned long _active       = 0;    \
+    unsigned long _instr        = 0;    \
+    unsigned long _ldstall      = 0;    \
+    unsigned long _jrstall      = 0;    \
+    unsigned long _imiss        = 0;    \
+    unsigned long _ld           = 0;    \
+    unsigned long _st           = 0;    \
+    unsigned long _jump         = 0;    \
+    unsigned long _branch       = 0;    \
+    unsigned long _btaken       = 0;    \
+    unsigned long _rvc          = 0;    \
+    unsigned long _ld_ext       = 0;    \
+    unsigned long _st_ext       = 0;    \
+    unsigned long _ld_ext_cyc   = 0;    \
+    unsigned long _st_ext_cyc   = 0;    \
+    unsigned long _tcdm_cont    = 0;    \
+                                        \
     for (int _k = 0; _k < HOTTING + REPEAT; _k++) { \
         pi_perf_conf( (1<<PI_PERF_CYCLES)           \
                     | (1<<PI_PERF_ACTIVE_CYCLES)    \
                     | (1<<PI_PERF_INSTR)            \
                     | (1<<PI_PERF_LD_STALL)         \
+                    | (1<<PI_PERF_JR_STALL)         \
                     | (1<<PI_PERF_IMISS)            \
                     | (1<<PI_PERF_LD)               \
-                    | (1<<PI_PERF_ST));             \
+                    | (1<<PI_PERF_ST)               \
+                    | (1<<PI_PERF_JUMP)             \
+                    | (1<<PI_PERF_BRANCH)           \
+                    | (1<<PI_PERF_BTAKEN)           \
+                    | (1<<PI_PERF_RVC)              \
+                    | (1<<PI_PERF_LD_EXT)           \
+                    | (1<<PI_PERF_ST_EXT)           \
+                    | (1<<PI_PERF_LD_EXT_CYC)       \
+                    | (1<<PI_PERF_ST_EXT_CYC)       \
+                    | (1<<PI_PERF_TCDM_CONT)        \
+                    );                              \
 
 /**
  * @brief Resets and starts the performance counters.
@@ -53,23 +82,107 @@
             _active     += pi_perf_read(PI_PERF_ACTIVE_CYCLES); \
             _instr      += pi_perf_read(PI_PERF_INSTR);         \
             _ldstall    += pi_perf_read(PI_PERF_LD_STALL);      \
+            _jrstall    += pi_perf_read(PI_PERF_JR_STALL);      \
             _imiss      += pi_perf_read(PI_PERF_IMISS);         \
             _ld         += pi_perf_read(PI_PERF_LD);            \
             _st         += pi_perf_read(PI_PERF_ST);            \
+            _jump       += pi_perf_read(PI_PERF_JUMP);          \
+            _branch     += pi_perf_read(PI_PERF_BRANCH);        \
+            _btaken     += pi_perf_read(PI_PERF_BTAKEN);        \
+            _rvc        += pi_perf_read(PI_PERF_RVC);           \
+            _ld_ext     += pi_perf_read(PI_PERF_LD_EXT);        \
+            _st_ext     += pi_perf_read(PI_PERF_ST_EXT);        \
+            _ld_ext_cyc += pi_perf_read(PI_PERF_LD_EXT_CYC);    \
+            _st_ext_cyc += pi_perf_read(PI_PERF_ST_EXT_CYC);    \
+            _tcdm_cont  += pi_perf_read(PI_PERF_TCDM_CONT);     \
         }                                                       \
                                                                 \
-        if (_k == HOTTING + REPEAT - 1) {                                               \
-            int id = pi_core_id();                                                      \
-            printf("[%d] number of cycles = %lu\n", id, _cycles/REPEAT);                \
-            printf("[%d] number of active cycles = %lu\n", id, _active/REPEAT);         \
-            printf("[%d] number of instruction executed = %lu\n", id, _instr/REPEAT);   \
-            printf("[%d] number of load stall = %lu\n", id, _ldstall/REPEAT);           \
-            printf("[%d] number of instruction misses = %lu\n", id, _imiss/REPEAT);     \
-            printf("[%d] number of data stores = %lu\n", id, _st/REPEAT);               \
-            printf("[%d] CPI = %f\n", id, (float) _cycles/_instr);                      \
-        }                                                                               \
-                                                                                        \
+        if (_k == HOTTING + REPEAT - 1) {                           \
+            print_stats(_cycles, _active, _instr, _ldstall,         \
+                        _jrstall, _imiss, _ld, _st, _jump,          \
+                        _branch, _btaken, _rvc, _ld_ext, _st_ext,   \
+                        _ld_ext_cyc, _st_ext_cyc, _tcdm_cont);      \
+        }                                                           \
+                                                                    \
     }
+
+#else   /* CLUSTER */
+
+/**
+ * @brief Initializes performance counters and begins the measurement loop.
+ *
+ * @note This macro must be called first, before START_STATS() and STOP_STATS().
+ */
+#define INIT_STATS()    \
+    unsigned long _cycles       = 0;    \
+    unsigned long _active       = 0;    \
+    unsigned long _instr        = 0;    \
+    unsigned long _ldstall      = 0;    \
+    unsigned long _jrstall      = 0;    \
+    unsigned long _imiss        = 0;    \
+    unsigned long _ld           = 0;    \
+    unsigned long _st           = 0;    \
+    unsigned long _jump         = 0;    \
+    unsigned long _branch       = 0;    \
+    unsigned long _btaken       = 0;    \
+    unsigned long _rvc          = 0;    \
+                                        \
+    for (int _k = 0; _k < HOTTING + REPEAT; _k++) { \
+        pi_perf_conf( (1<<PI_PERF_CYCLES)           \
+                    | (1<<PI_PERF_ACTIVE_CYCLES)    \
+                    | (1<<PI_PERF_INSTR)            \
+                    | (1<<PI_PERF_LD_STALL)         \
+                    | (1<<PI_PERF_JR_STALL)         \
+                    | (1<<PI_PERF_IMISS)            \
+                    | (1<<PI_PERF_LD)               \
+                    | (1<<PI_PERF_ST)               \
+                    | (1<<PI_PERF_JUMP)             \
+                    | (1<<PI_PERF_BRANCH)           \
+                    | (1<<PI_PERF_BTAKEN)           \
+                    | (1<<PI_PERF_RVC)              \
+                    );                              \
+
+/**
+ * @brief Resets and starts the performance counters.
+ *
+ * @note This macro must be called after INIT_STATS() within the measurement loop.
+ */
+#define START_STATS()       \
+        pi_perf_reset();    \
+        pi_perf_start();
+
+/**
+ * @brief Stops the performance counters, accumulates, and prints statistics.
+ *
+ * @note This macro must be called after START_STATS() within the measurement loop.
+ */
+#define STOP_STATS()    \
+        pi_perf_stop(); \
+                        \
+        if (_k >= HOTTING) {                                    \
+            _cycles     += pi_perf_read(PI_PERF_CYCLES);        \
+            _active     += pi_perf_read(PI_PERF_ACTIVE_CYCLES); \
+            _instr      += pi_perf_read(PI_PERF_INSTR);         \
+            _ldstall    += pi_perf_read(PI_PERF_LD_STALL);      \
+            _jrstall    += pi_perf_read(PI_PERF_JR_STALL);      \
+            _imiss      += pi_perf_read(PI_PERF_IMISS);         \
+            _ld         += pi_perf_read(PI_PERF_LD);            \
+            _st         += pi_perf_read(PI_PERF_ST);            \
+            _jump       += pi_perf_read(PI_PERF_JUMP);          \
+            _branch     += pi_perf_read(PI_PERF_BRANCH);        \
+            _btaken     += pi_perf_read(PI_PERF_BTAKEN);        \
+            _rvc        += pi_perf_read(PI_PERF_RVC);           \
+        }                                                       \
+                                                                \
+        if (_k == HOTTING + REPEAT - 1) {                           \
+            print_stats(_cycles, _active, _instr, _ldstall,         \
+                        _jrstall, _imiss, _ld, _st, _jump,          \
+                        _branch, _btaken, _rvc, 0, 0, 0, 0, 0);     \
+        }                                                           \
+                                                                    \
+    }
+
+#endif  /* CLUSTER */
 
 #else   /* STATS */
 
