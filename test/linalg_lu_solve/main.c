a508dfb8abbe69250f48a3617d5d25efb9d72967
callme-sam <samuele.colmi@chips.it>
2025-09-02 10:10:19 +0200

    test/linalg_lu_solve: Add test case for linalg_lu_solve
    Implement test case for linalg_lu_solve, together with its python script
    to generate test data

---
+++ test/linalg_lu_solve/main.c
@@ -0,0 +1,148 @@
+#include <stdbool.h>
+
+#include "data.h"
+#include "play.h"
+#include "stats.h"
+#include "utils.h"
+
+#include "pmsis.h"
+
+PI_L1 float mat[DIM_M * DIM_M];
+PI_L1 float vec[DIM_M];
+PI_L1 int perm[DIM_M];
+PI_L1 float result[DIM_M];
+
+static void initialize_data()
+{
+    if (!is_master_core())
+        return;
+
+    for (int m = 0; m < DIM_M; m++){
+        for (int n = 0; n < DIM_M; n++)
+            mat[m * DIM_M + n] = LU[m * DIM_M + n];
+    }
+
+    for (int m = 0; m < DIM_M; m++) {
+        vec[m] = b[m];
+        perm[m] = p[m];
+        result[m] = 0;
+    }
+}
+
+static void check_result()
+{
+    bool test_result;
+
+    if (!is_master_core())
+        return;
+
+    test_result = vector_compare(result, expected, DIM_M);
+
+    matrix_print(mat, DIM_M, DIM_M, "mat");
+    vector_print((float *)perm, DIM_M, "perm");
+    vector_print(vec, DIM_M, "vec");
+    vector_print(result, DIM_M, "result");
+    vector_print(expected, DIM_M, "expected");
+    printf("INFO | Test %s\n", test_result ? "SUCCESS" : "FAILED");
+}
+
+static void run_test()
+{
+    initialize_data();
+    barrier();
+
+    INIT_STATS();
+    START_STATS();
+    linalg_lu_solve(mat, vec, perm, result, DIM_M, DIM_M);
+    STOP_STATS();
+
+    barrier();
+    check_result();
+}
+
+#ifdef  CLUSTER
+
+static void cluster_entry()
+{
+    pi_cl_team_fork(NUM_CORES, run_test, NULL);
+}
+
+static int run_test_on_cluster()
+{
+    struct pi_cluster_conf cl_conf;
+    struct pi_cluster_task cl_task;
+    struct pi_device cluster_dev;
+    int ret;
+
+    pi_cluster_conf_init(&cl_conf);
+    pi_open_from_conf(&cluster_dev, &cl_conf);
+
+    ret = pi_cluster_open(&cluster_dev);
+    if (ret) {
+        printf("ERROR | Unable to open cluster device\n");
+        goto exit;
+    }
+
+    pi_cluster_task(&cl_task, cluster_entry, NULL);
+    ret = pi_cluster_send_task_to_cl(&cluster_dev, &cl_task);
+    if (ret) {
+        printf("ERROR | Unable to send task to cluster controller\n");
+        goto exit;
+    }
+
+    pi_cluster_close(&cluster_dev);
+
+exit:
+    return ret;
+}
+
+static int test_linalg_lu_solve()
+{
+    int ret;
+
+    printf("INFO | Running 'linalg_lu_solve' test on PULP Cluster with %d cores\n", NUM_CORES);
+
+    ret = run_test_on_cluster();
+    if (ret)
+        printf("ERROR | Unable to run test on cluster\n");
+
+    return ret;
+}
+
+#else   /* CLUSTER */
+
+static int run_test_on_fabric()
+{
+    run_test();
+    return 0;
+}
+
+static int test_linalg_lu_solve()
+{
+    int ret;
+
+    printf("INFO | Running 'linalg_lu_solve' test on Fabric Controller\n");
+
+    ret = run_test_on_fabric();
+    if (ret)
+        printf("ERROR | Unable to run test on FC\n");
+
+    return ret;
+}
+
+#endif  /* CLUSTER */
+
+static void test_kickoff()
+{
+    int ret;
+
+    printf("\n##################################### LINALG_LU_SOLVE TEST ####################################\n\n");
+    ret = test_linalg_lu_solve();
+    printf("\n##########################################################################################\n\n");
+    pmsis_exit(ret);
+}
+
+int main()
+{
+    return pmsis_kickoff(test_kickoff);
+}
---
+++ test/linalg_lu_solve/test_data/generator.py
@@ -0,0 +1,101 @@
+import os
+import sys
+import numpy as np
+from scipy.linalg import lu, solve_triangular
+
+def parse_args():
+    """Parse command line arguments for matrix dimensions."""
+    if len(sys.argv) != 2:
+        print("Error: missing arguments <M>")
+        print("Usage: python3 generator.py <M>")
+        sys.exit(1)
+
+    try:
+        M = int(sys.argv[1])
+    except ValueError:
+        print("Error: <M> must be integers")
+        print("Usage: python3 generator.py <M>")
+        sys.exit(1)
+
+    return M
+
+def generate_solvable_system(n):
+    """Generate a a solvable linear system Ax = b with a square matrix."""
+    A = np.random.randn(n, n)
+    A += np.diag(np.full(n, 50.0))  # Adds a dominant diagonal for better stability
+    x = np.random.randn(n)
+    b = A @ x
+    return A, x, b
+
+def lu_decomp(mat):
+    """Performs LU decomposition of matix mat with partial pivoting"""
+
+    rows, cols = mat.shape
+    dim_min = min(rows, cols)
+
+    perm, L, U = lu(mat, permute_l=False, p_indices=True)
+
+    dst = np.copy(mat)
+
+    dst[:dim_min, :dim_min] = U
+    for i in range(rows):
+        for j in range(dim_min):
+            if (i > j):
+                dst[i, j] = L[i, j]
+
+    return dst, perm, L, U
+
+def lu_solve(L, U, b, perm):
+    """Solves linear system Ax = b using LU factorization"""
+    b_perm = b[perm]
+    y = solve_triangular(L, b_perm, lower=True)
+    x = solve_triangular(U, y, lower=False)
+    return x
+
+
+def format_matrix(array):
+    """Convert a NumPy matrix into a C-style array."""
+    # Flatten the array for C representation
+    flat_array = array.flatten()
+    return "{ " + ", ".join(f"{x:f}" for x in flat_array) + " }"
+
+def format_array(array):
+    """Convert a NumPy array into a C-style array."""
+    return "{ " + ", ".join(f"{x}" for x in array) + " }"
+
+def format_float(value):
+    """Convert a float value into a C-style representation."""
+    return f"{value:f}"
+
+def generate_header_file(M, mat, b, perm, expected, filename="data.h"):
+    """Generate a C header file containing the matrices and their expected result."""
+
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    filepath = os.path.join(script_dir, filename)
+
+    with open(filepath, "w") as f:
+        f.write("#ifndef DATA_H_\n")
+        f.write("#define DATA_H_\n\n")
+        f.write('#include "pmsis.h"\n\n')
+
+        f.write(f"#define DIM_M {M}\n\n")
+
+        f.write(f"PI_L2 float LU[] = {format_matrix(mat)};\n")
+        f.write(f"PI_L2 float b[] = {format_array(b)};\n")
+        f.write(f"PI_L2 int p[] = {format_array(perm)};\n")
+        f.write(f"PI_L2 float expected[] = {format_matrix(expected)};\n\n")
+
+        f.write("#endif  /* DATA_H_ */\n")
+
+def main():
+    M = parse_args()
+    A, x, b = generate_solvable_system(M)
+
+    lu, perm, L, U = lu_decomp(A)
+    expected = lu_solve(L, U, b, perm)
+
+    generate_header_file(M, lu, b, perm, expected)
+    print(f"File 'data.h' successfully generated for matrices of size {M}x{M}.")
+
+if __name__ == "__main__":
+    main()
