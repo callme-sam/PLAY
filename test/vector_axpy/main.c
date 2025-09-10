d3622ce0edfb49cfae6bb073c40349fe35bcc596
callme-sam <samuele.colmi@chips.it>
2025-09-10 15:47:22 +0200

    vector_axpy: Optimization

--- test/vector_axpy/main.c
+++ test/vector_axpy/main.c
@@ -7,10 +7,10 @@
 
 #include "pmsis.h"
 
-PI_L1 float src_a[LEN];
-PI_L1 float src_b[LEN];
+PI_L1 float src_a[LEN] __attribute__((aligned(4)));
+PI_L1 float src_b[LEN] __attribute__((aligned(4)));
 PI_L1 float alpha;
-PI_L1 float result[LEN];
+PI_L1 float result[LEN] __attribute__((aligned(4)));
 
 static void initialize_vectors()
 {
@@ -44,12 +44,13 @@ static void check_result()
 
 static void run_test()
 {
+    volatile int len = LEN;
     initialize_vectors();
     barrier();
 
     INIT_STATS();
     START_STATS();
-    vector_axpy(src_a, src_b, alpha, result, LEN);
+    vector_axpy(src_a, src_b, alpha, result, len);
     STOP_STATS();
 
     barrier();
