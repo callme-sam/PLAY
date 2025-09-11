e07ba7d653019df2b610f9e60b8f1c7733b72da7
callme-sam <samuele.colmi@chips.it>
2025-09-11 16:24:22 +0200

    test/matrix_mul: Optimization analisys

--- source/matrix_mul/matrix_mul.c
+++ source/matrix_mul/matrix_mul.c
@@ -19,14 +19,166 @@ int matrix_mul_parallel(const float *src_a, const float *src_b, float *dst, cons
     row_start = id * block + (id < left ? id : left);
     row_end = row_start + block + (id < left ? 1 : 0);
 
+#if 0
+    /* for + unroll */
+    /* 1C - cycl: - stall: - tcdm: - ipc: */
+    /* 8C - cycl: - stall: - tcdm: - ipc: */
     for (int m = row_start; m < row_end; m++) {
         for (int p = 0; p < dim_P; p++) {
-            sum = 0;
-            for (int n = 0; n < dim_N; n++)
-                sum += src_a[m * dim_N + n] * src_b[n * dim_P + p];
-            dst[m * dim_P + p] = sum;
+            float sum1 = 0;
+            float sum2 = 0;
+            for (int n = 0; (n + 1) < dim_N; n += 2) {
+                float src_a1, src_a2;
+                float src_b1, src_b2;
+
+                src_a1 = src_a[m * dim_N + n];
+                src_a2 = src_a[m * dim_N + (n + 1)];
+                src_b1 = src_b[n * dim_P + p];
+                src_b2 = src_b[(n + 1) * dim_P + p];
+
+                sum1 += src_a1 * src_b1;
+                sum2 += src_a2 * src_b2;
+            }
+            dst[m * dim_P + p] = sum1 + sum2;
         }
     }
+
+    if (dim_N % 2) {
+        for (int m = row_start; m < row_end; m++) {
+            for (int p = 0; p < dim_P; p++) {
+                float a;
+                float b;
+
+                a = src_a[m * dim_N + (dim_N - 1)];
+                b = src_b[(dim_N - 1) * dim_P + p];
+
+                dst[m * dim_P + p] += a * b;
+            }
+        }
+    }
+#endif
+
+#if 0
+    /* do-while + unroll */
+    /* 1C - cycl: - stall: - tcdm: - ipc: */
+    /* 8C - cycl: - stall: - tcdm: - ipc: */
+    int m, n, p;
+    float sum1, sum2;
+
+    m = row_start;
+    do {
+        p = 0;
+        do {
+            sum1 = 0;
+            sum2 = 0;
+            n = 0;
+            do {
+                float src_a1, src_a2;
+                float src_b1, src_b2;
+
+                src_a1 = src_a[m * dim_N + n];
+                src_a2 = src_a[m * dim_N + (n + 1)];
+                src_b1 = src_b[n * dim_P + p];
+                src_b2 = src_b[(n + 1) * dim_P + p];
+
+                sum1 += src_a1 * src_b1;
+                sum2 += src_a2 * src_b2;
+
+                n += 2;
+            } while ((n + 1) < dim_N);
+
+            dst[m * dim_P + p] = sum1 + sum2;
+            p++;
+        } while (p < dim_P);
+
+        m++;
+    } while (m < row_end);
+
+    if (dim_N % 2) {
+        m = row_start;
+        do {
+            p = 0;
+            do {
+                float a;
+                float b;
+
+                a = src_a[m * dim_N + (dim_N - 1)];
+                b = src_b[(dim_N - 1) * dim_P + p];
+
+                dst[m * dim_P + p] += a * b;
+                p++;
+            } while(p < dim_P);
+
+            m++;
+        } while(m < row_end);
+    }
+
+#endif
+
+#if 1
+    /* do-while single step + unroll */
+    /* 1C - cycl: - stall: - tcdm: - ipc: */
+    /* 8C - cycl: - stall: - tcdm: - ipc: */
+    int num_ops;
+    int rem_ops;
+    int m, n, p;
+    float sum1, sum2;
+
+
+    rem_ops = dim_N % 2;
+    m = row_start;
+    do {
+        p = 0;
+        do {
+            num_ops = dim_N / 2;
+            sum1 = 0;
+            sum2 = 0;
+            n = 0;
+            do {
+                float src_a1, src_a2;
+                float src_b1, src_b2;
+
+                src_a1 = src_a[m * dim_N + n];
+                src_a2 = src_a[m * dim_N + (n + 1)];
+                src_b1 = src_b[n * dim_P + p];
+                src_b2 = src_b[(n + 1) * dim_P + p];
+
+                sum1 += src_a1 * src_b1;
+                sum2 += src_a2 * src_b2;
+
+                n += 2;
+                num_ops--;
+            } while (num_ops > 0);
+
+            dst[m * dim_P + p] = sum1 + sum2;
+            p++;
+        } while (p < dim_P);
+
+        m++;
+    } while (m < row_end);
+
+    if (rem_ops) {
+        m = row_start;
+        do {
+            p = 0;
+            do {
+                float a;
+                float b;
+
+                a = src_a[m * dim_N + (dim_N - 1)];
+                b = src_b[(dim_N - 1) * dim_P + p];
+
+                dst[m * dim_P + p] += a * b;
+                p++;
+            } while (p < dim_P);
+            m++;
+        } while (m < row_end);
+    }
+#endif
+
+#if NUM_CORES > 1
+    pi_cl_team_barrier();
+#endif
 
     return 0;
 }
@@ -51,7 +203,7 @@ int matrix_mul_serial(const float *src_a, const float *src_b, float *dst, const 
 
 #endif  /* CLUSTER */
 
-int matrix_mul(const float *src_a, const float *src_b, float *dst, const int dim_M, const int dim_N, const int dim_P)
+__attribute__((noinline))int matrix_mul(const float *src_a, const float *src_b, float *dst, const int dim_M, const int dim_N, const int dim_P)
 {
     int ret;
 
---
+++ test/matrix_mul/matrix_mul Optimization Notes.md
@@ -0,0 +1,34 @@
+﻿# Analysis of Matrix Multiplication Implementations
+
+Performance analisys of the three implementations (for, do-while, and do-while single-step) for matrix multiplication, considering both 1-core and 8-core results.
+
+---
+
+## 1 Core (1C)
+
+When using a single core, the **do-while single-step** implementation is the most efficient in terms of total cycles.
+
+* **do-while single-step**: 447,332 cycles
+* **do-while**: 459,611 cycles
+* **for**: 467,930 cycles
+
+The difference between the **`for`** and **`do-while`** implementations is minimal, but the **`do-while single-step`** shows a notable improvement, requiring about **4.4% fewer cycles** compared to the `for` implementation.
+
+---
+
+## 8 Cores (8C)
+
+With 8 cores the **`for`** and **`do-while`** implementations are almost identical in terms of cycles, while the **`do-while single-step`** remains the highest-performing option.
+
+* **do-while single-step**: 3,238 cycles
+* **for**: 4,414 cycles
+* **do-while**: 4,417 cycles
+
+In this multi-core scenario, the **`do-while single-step`** is once again the fastest implementation, with about **26.6% fewer cycles** than the other two. This suggests that, while all implementations benefit from parallelism, the loop unrolling with a "single-step" approach further reduces the number of instructions and execution cycles per core, leading to superior efficiency.
+
+---
+
+## Conclusion
+
+* **Best Implementation**: The **`do-while single-step`** implementation offers the best performance in both scenarios (1 core and 8 cores). This indicates that specific loop optimization has a significant impact on performance, especially when computation is distributed across multiple cores.
+* **Minimal Differences (8C)**: The **`for`** and **`do-while`** implementations show a nearly negligible difference in a multi-core environment, suggesting that parallelism outweighs the minor inefficiencies between these two loop types.
