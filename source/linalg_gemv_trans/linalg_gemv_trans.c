abf785c81e60470026aea476c38745ecc64862e7
callme-sam <samuele.colmi@chips.it>
2025-09-15 17:43:11 +0200

    linalg_gemv_trans: Optimization

--- source/linalg_gemv_trans/linalg_gemv_trans.c
+++ source/linalg_gemv_trans/linalg_gemv_trans.c
@@ -4,14 +4,45 @@
 
 #ifdef  CLUSTER
 
+static inline void mat_vec_mul(const float *mat, const float *vec_x, const int n, const int dim_M, const int dim_N, float *sum1, float *sum2)
+{
+    int num_ops;
+    int ops;
+    int m;
+
+    m = 0;
+    ops = 0;
+    *sum1 = 0;
+    *sum2 = 0;
+    num_ops = dim_M / 2;
+    do {
+        float mat1, mat2;
+        float vec1, vec2;
+
+        mat1 = mat[m * dim_N + n];
+        mat2 = mat[(m + 1) * dim_N + n];
+        vec1 = vec_x[m];
+        vec2 = vec_x[m + 1];
+
+        *sum1 += mat1 * vec1;
+        *sum2 += mat2 * vec2;
+
+        m += 2;
+        ops++;
+    } while (ops < num_ops);
+}
+
 int linalg_gemv_trans_parallel(const float *mat, const float *vec_x, const float *vec_y, const float alpha, const float beta, float *dst, const int dim_M, const int dim_N)
 {
     int row_start;
     int row_end;
-    float sum;
+    int rem_ops;
+    float sum1;
+    float sum2;
     int block;
     int left;
     int id;
+    int n;
 
     if (alpha == 0.0f) {
         if (beta == 0.0f)
@@ -28,13 +59,31 @@ int linalg_gemv_trans_parallel(const float *mat, const float *vec_x, const float
     left = dim_N % NUM_CORES;
     row_start = id * block + (id < left ? id : left);
     row_end = row_start + block + (id < left ? 1 : 0);
+    rem_ops = dim_M % 2;
 
-    for (int n = row_start; n < dim_N; n++) {
-        sum = 0;
-        for (int m = 0; m < dim_M; m++)
-            sum += mat[m * dim_N + n] * vec_x[m];
-        dst[n] = alpha * sum + beta * vec_y[n];
+    n = row_start;
+    if (rem_ops) {
+        /* loop unroll will have leftovers */
+        do {
+            mat_vec_mul(mat, vec_x, n, dim_M, dim_N, &sum1, &sum2);
+            /* leftover */
+            sum1 += mat[(dim_M - 1) * dim_N + n] * vec_x[dim_M - 1];
+            dst[n] = alpha * (sum1 + sum2) + beta * vec_y[n];
+            n++;
+        } while (n < row_end);
+
+    } else {
+        /* loop unroll will NOT have leftovers */
+        do {
+            mat_vec_mul(mat, vec_x, n, dim_M, dim_N, &sum1, &sum2);
+            dst[n] = alpha * (sum1 + sum2) + beta * vec_y[n];
+            n++;
+        } while (n < row_end);
     }
+
+#if NUM_CORES > 1
+    pi_cl_team_barrier();
+#endif
 
     return 0;
 }
@@ -67,7 +116,7 @@ int linalg_gemv_trans_serial(const float *mat, const float *vec_x, const float *
 #endif  /* CLUSTER */
 
 
-int linalg_gemv_trans(const float *mat, const float *vec_x, const float *vec_y, const float alpha, const float beta, float *dst, const int dim_M, const int dim_N)
+__attribute__((noinline)) int linalg_gemv_trans(const float *mat, const float *vec_x, const float *vec_y, const float alpha, const float beta, float *dst, const int dim_M, const int dim_N)
 {
     int ret;
 
