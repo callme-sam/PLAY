b82425794172f0764531106f1960a9373f63a9e7
callme-sam <219649100+callme-sam@users.noreply.github.com>
2025-10-03 10:47:55 +0200

    test/linalgs_cholesky_decomp: Add test case for cholesky decomp

---
+++ test/linalg_cholesky_decomp/main.c
@@ -0,0 +1,171 @@
+#include "data.h"
+#include "play.h"
+#include "stats.h"
+#include "utils.h"
+
+#include <stdbool.h>
+#include <math.h>
+
+#include "pmsis.h"
+
+PI_L1 float src[DIM_M * DIM_M]  __attribute__((aligned(4)));
+PI_L1 float result[DIM_M * DIM_M] __attribute((aligned(4)));
+
+static void initialize_data()
+{
+    if (!is_master_core())
+        return;
+
+    for (int m = 0; m < DIM_M; m++) {
+        for (int n = 0; n < DIM_M; n++) {
+            src[m * DIM_M + n] = mat[m * DIM_M + n];
+            result[m * DIM_M + n] = 0;
+        }
+    }
+}
+
+static void check_result()
+{
+    bool test_result;
+    bool res_cmp;
+    bool s_cmp;
+    bool v_cmp;
+
+    if (!is_master_core())
+        return;
+
+    test_result = matrix_compare(result, expected, DIM_M, DIM_M);
+
+#ifdef  PRINT_DATA
+    matrix_print(src, DIM_M, DIM_M, "mat");
+    matrix_print(result, DIM_M, DIM_M, "result");
+    matrix_print(expected, DIM_M, DIM_M, "expected");
+#endif  /* PRINT_DATA */
+
+    printf("INFO | Test %s\n", test_result ? "SUCCESS" : "FAILED");
+}
+
+static void run_test()
+{
+    volatile int m = DIM_M;
+    initialize_data();
+    barrier();
+
+    INIT_STATS();
+    START_LOOP_STATS();
+    START_STATS();
+    linalg_cholesky_decomp(src, result, m);
+    STOP_STATS();
+    END_LOOP_STATS();
+
+    barrier();
+    check_result();
+}
+
+#ifdef  CLUSTER
+
+static void cluster_entry()
+{
+    pi_cl_team_fork(NUM_CORES, run_test, NULL);
+}
+
+static int run_test_on_cluster()
+{
+    struct pi_cluster_conf cl_conf;
+    struct pi_cluster_task cl_task;
+    struct pi_device cluster_dev;
+    int ret;
+
+    pi_cluster_conf_init(&cl_conf);
+    pi_open_from_conf(&cluster_dev, &cl_conf);
+
+    ret = pi_cluster_open(&cluster_dev);
+    if (ret) {
+#ifdef  ENABLE_LOGGING
+        printf("ERROR | Unable to open cluster device\n");
+#endif  /* ENABLE_LOGGING */
+        goto exit;
+    }
+
+    pi_cluster_task(&cl_task, cluster_entry, NULL);
+    ret = pi_cluster_send_task_to_cl(&cluster_dev, &cl_task);
+    if (ret) {
+#ifdef  ENABLE_LOGGING
+        printf("ERROR | Unable to send task to cluster controller\n");
+#endif  /* ENABLE_LOGGING */
+        goto exit;
+    }
+
+    pi_cluster_close(&cluster_dev);
+
+exit:
+    return ret;
+}
+
+static int test_linalg_cholesky_decomp()
+{
+    int ret;
+
+#ifdef  ENABLE_LOGGING
+    printf("INFO | Running 'linalg_cholesky_decomp' test on PULP Cluster with %d cores\n", NUM_CORES);
+#endif  /* ENABLE_LOGGING */
+
+    ret = run_test_on_cluster();
+
+#ifdef  ENABLE_LOGGING
+    if (ret)
+        printf("ERROR | Unable to run test on cluster\n");
+#endif  /* ENABLE_LOGGING */
+
+    return ret;
+}
+
+#else   /* CLUSTER */
+
+static int run_test_on_fabric()
+{
+    run_test();
+    return 0;
+}
+
+static int test_linalg_cholesky_decomp()
+{
+    int ret;
+
+#ifdef  ENABLE_LOGGING
+    printf("INFO | Running 'linalg_cholesky_decomp' test on Fabric Controller\n");
+#endif  /* ENABLE_LOGGING */
+
+    ret = run_test_on_fabric();
+
+#ifdef  ENABLE_LOGGING
+    if (ret)
+        printf("ERROR | Unable to run test on FC\n");
+#endif  /* ENABLE_LOGGING */
+
+    return ret;
+}
+
+#endif  /* CLUSTER */
+
+static void test_kickoff()
+{
+    int ret;
+
+#ifdef  ENABLE_LOGGING
+    printf("\n##################################### LINALG_CHOLESKY_DECOMP TEST ####################################\n\n");
+#endif  /* ENABLE_LOGGING */
+
+    ret = test_linalg_cholesky_decomp();
+
+#ifdef  ENABLE_LOGGING
+    printf("\n##########################################################################################\n\n");
+#endif  /* ENABLE_LOGGING */
+
+    pmsis_exit(ret);
+}
+
+int main()
+{
+    return pmsis_kickoff(test_kickoff);
+}
---
+++ test/linalg_cholesky_decomp/test_data/generator.py
@@ -0,0 +1,72 @@
+import os
+import sys
+import numpy as np
+from scipy.linalg import eigh
+
+def parse_args():
+    """Parse command line arguments for matrix dimensions."""
+    if len(sys.argv) != 2:
+        print("Error: missing arguments <M>")
+        print("Usage: python3 generator.py <M>")
+        sys.exit(1)
+
+    try:
+        M = int(sys.argv[1])
+    except ValueError:
+        print("Error: <M> must be integer")
+        print("Usage: python3 generator.py <M>")
+        sys.exit(1)
+
+    return M
+
+def generate_matrix(dim):
+    """Generate a square matrix of floats with given dimensions."""
+    return np.random.randn(dim, dim)
+
+def get_sdp_mat(mat, eps=1e-6):
+    """Generate a symmetric positive definite matrix"""
+    mat =  mat @ mat.T
+    mat += np.eye(mat.shape[0]) * eps
+    mat = (mat + mat.T) / 2
+    return mat
+
+def cholesky_decomp(mat):
+    """Computes the decomposition of a symmetric and definite positive matrix using the Cholesky algorithm"""
+    return np.linalg.cholesky(mat)
+
+def format_matrix(array):
+    """Convert a NumPy matrix into a C-style array."""
+    # Flatten the array for C representation
+    flat_array = array.flatten()
+    return "{ " + ", ".join(f"{x:f}" for x in flat_array) + " }"
+
+def generate_header_file(M, mat, expected, filename="data.h"):
+    """Generate a C header file containing the matrices and their expected result."""
+
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    filepath = os.path.join(script_dir, filename)
+
+    with open(filepath, "w") as f:
+        f.write("#ifndef DATA_H_\n")
+        f.write("#define DATA_H_\n\n")
+        f.write('#include "pmsis.h"\n\n')
+
+        f.write(f"#define DIM_M {M}\n\n")
+
+        f.write(f"PI_L2 float mat[] = {format_matrix(mat)};\n")
+        f.write(f"PI_L2 float expected[] = {format_matrix(expected)};\n\n")
+
+        f.write("#endif  /* DATA_H_ */\n")
+
+def main():
+    M = parse_args()
+    mat = generate_matrix(M)
+    mat_sdp = get_sdp_mat(mat)
+
+    expected = cholesky_decomp(mat_sdp)
+
+    generate_header_file(M, mat_sdp, expected)
+    print(f"File 'data.h' successfully generated for matrices of size {M}x{M}.")
+
+if __name__ == "__main__":
+    main()
