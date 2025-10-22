08232d5cb9ceb3f0a533e6b8e7883e714b8e09f1
callme-sam <219649100+callme-sam@users.noreply.github.com>
2025-10-22 16:37:22 +0200

    vector_mul_spatz: Add support for SPATZ CLUSTER

--- source/vector_mul/arch/vector_mul_spatz.c
+++ source/vector_mul/arch/vector_mul_spatz.c
@@ -4,47 +4,58 @@
 #include "play.h"
 
 #include "snrt.h"
+#include "printf.h"
 
 int vector_mul_spatz(const float *src_a, const float *src_b, float *dst, const int len)
 {
-    int remaining = len;
+    int start;
+    int block;
+    int left;
+    int end;
+    int id;
+
+    size_t cc_avl;
+    size_t avl;
     size_t vl;
 
-    const float *a = src_a;
-    const float *b = src_b;
-    float *d = dst;
+    const float *a;
+    const float *b;
+    float *d;
 
-    while (remaining > 0) {
-        asm volatile (
-            "vsetvli %0, %1, e32, m1, ta, ma"
-            : "=r"(vl)
-            : "r"(remaining)
-        );
+    id = snrt_cluster_core_idx();
+    block = len / NUM_CC;
+    left = len % NUM_CC;
 
-        asm volatile (
-            "vle32.v v0, (%0)"
-            :: "r"(a)
-        );
-        asm volatile (
-            "vle32.v v1, (%0)"
-            :: "r"(b)
-        );
+    start = id * block + (id < left ? id : left);
+    end = start + block + (id < left ? 1 : 0);
+    cc_avl = end - start;
+    avl = cc_avl;
 
-        asm volatile (
-            "vfmul.vv v2, v0, v1"
-        );
+    a = src_a + start;
+    b = src_b + start;
+    d = dst + start;
 
-        asm volatile (
-            "vse32.v v2, (%0)"
-            :: "r"(d)
-        );
+    for (; avl > 0; avl -= vl) {
+        asm volatile ("vsetvli %0, %1, e32, m1, ta, ma" : "=r"(vl) : "r"(avl));
+
+        asm volatile ("vle32.v v0, (%0)" :: "r"(a));
+        asm volatile ("vle32.v v1, (%0)" :: "r"(b));
+
+        asm volatile("vfmul.vv v2, v0, v1");
+
+        asm volatile("vse32.v v2, (%0)" :: "r"(d));
 
         a += vl;
         b += vl;
         d += vl;
-
-        remaining -= vl;
     }
+
+#if NUM_CC > 1
+    snrt_cluster_hw_barrier();
+#endif
+
+return 0;
+
 }
 
 #endif  /* TARGET_IS_SPATZ */
