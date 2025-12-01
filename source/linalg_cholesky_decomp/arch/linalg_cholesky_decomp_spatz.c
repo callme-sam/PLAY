d91dfb6388b39a2625662d275fc1314cab350f7d
callme-sam <219649100+callme-sam@users.noreply.github.com>
2025-12-01 16:38:54 +0100

    source/linalg_cholesky_decomp: Optimization

--- source/linalg_cholesky_decomp/arch/linalg_cholesky_decomp_spatz.c
+++ source/linalg_cholesky_decomp/arch/linalg_cholesky_decomp_spatz.c
@@ -5,18 +5,25 @@
 
 #include "snrt.h"
 
-static const float ONE_f = 1.0f;
-static const float ZERO_f = 0.0f;
-
 static int linalg_cholesky_decomp_spatz_serial(const float *src, float *dst, const int dim)
 {
-    size_t original_avl;
-    size_t avl;
+    float ONE_f = 1.0f;
+    float ZERO_f = 0.0f;
+
     size_t vl;
+    size_t avl;
+    size_t stride;
+    size_t original_avl;
 
     float *row_m;
     float *row_n;
+    float *ptr_dst;
+    const float *ptr_src;
+
     float sum;
+    float Lmm_inv;
+
+    stride = dim * sizeof(float);
 
     for (int m = 0; m < dim; m++) {
         row_m = dst + (m * dim);
@@ -46,38 +53,32 @@ static int linalg_cholesky_decomp_spatz_serial(const float *src, float *dst, con
         }
 
         dst[m * dim + m] = sqrtf(src[m * dim + m] - sum);
+        Lmm_inv = ONE_f / dst[m * dim + m];
 
-        for (int n = (m + 1); n < dim; n++) {
-            row_m = dst + (m * dim);
-            row_n = dst + (n * dim);
-            avl = m;
-            sum = 0;
+        for (int n = (m + 1); n < dim; n += vl) {
+            ptr_src = src + (n * dim + m);
+            ptr_dst = dst + (n * dim + m);
+            avl = dim - n;
 
-            if (m != 0) {
-                asm volatile ("vsetvli %0, %1, e32, m8, ta, ma" : "=r"(vl) : "r"(avl));
-                asm volatile ("vfmv.v.f v0, %0" :: "f"(ZERO_f));
-                asm volatile ("vfmv.v.f v8, %0" :: "f"(ZERO_f));
+            asm volatile ("vsetvli %0, %1, e32, m8, ta, ma" : "=r"(vl) : "r"(avl));
+            asm volatile ("vfmv.v.f v0, %0" :: "f"(ZERO_f));
 
-                for (; avl > 0; avl -= vl) {
-                    asm volatile ("vsetvli %0, %1, e32, m8, ta, ma" : "=r"(vl) : "r"(avl));
+            for (int k = 0; k < m; k++) {
+                row_m = dst + (m * dim + k);
+                row_n = dst + (n * dim + k);
 
-                    asm volatile ("vle32.v v16, (%0)" :: "r"(row_m));
-                    asm volatile ("vle32.v v24, (%0)" :: "r"(row_n));
+                asm volatile ("vlse32.v v16, (%0), %1" :: "r"(row_n), "r"(stride));
+                snrt_cluster_hw_barrier();
 
-                    asm volatile ("vfmacc.vv v0, v16, v24");
-
-                    row_m += vl;
-                    row_n += vl;
-                }
-
-                asm volatile ("vsetvli %0, %1, e32, m8, ta, ma" : "=r"(vl) : "r"(original_avl));
-                asm volatile ("vfredsum.vs v8, v0, v8");
-
-                asm volatile ("vfmv.f.s %0, v8" : "=f"(sum));
+                asm volatile ("vfmacc.vf v0, %0, v16" :: "f"(*row_m));
             }
 
-            dst[n * dim + m] = (ONE_f / dst[m * dim + m]) * (src[n * dim + m] - sum);
+            asm volatile ("vlse32.v v8, (%0), %1" :: "r"(ptr_src), "r"(stride));
             snrt_cluster_hw_barrier();
+
+            asm volatile ("vfsub.vv v8, v8, v0");
+            asm volatile ("vfmul.vf v0, v8, %0" :: "f"(Lmm_inv));
+            asm volatile ("vsse32.v v0, (%0), %1" :: "r"(ptr_dst), "r"(stride));
         }
     }
 
